a7d0c5630f9fa83d207b6d42e042915125002e88
milpo <aneeshkumar10@gmail.com>
2020-03-17 07:41:58 -0400

    time entry

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -12,7 +12,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository timeEntryRepository()
+    TimeEntryRepository timeEntryRepository()
     {
         return  new InMemoryTimeEntryRepository();
     }
